81f54e78496cb2918b289dd3c6f5c101c329320c
sshuai001 <2714192896@qq.com>
2026-03-20 23:02:16 +0800

    add test

---
+++ src/models/blog.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const blogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  author: {
+    type: String,
+    required: true,
+    minlength: 2
+  },
+  url: {
+    type: String,
+    required: true,
+    minlength: 10
+  },
+  likes: {
+    type: Number,
+    default: 0
+  }
+})
+
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
